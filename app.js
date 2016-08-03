ce7f5c0987be1fcd21f4e15e31b16fd3b4ed8517
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-03 18:00:41 +0800

    加入post

--- app.js
+++ app.js
@@ -56,7 +56,11 @@ app.get('/', function (req, res) {
 
     request('http://x.rce.tw/s/h3584935/get_long_token.php?token=' + token, function (error, response, body) {
         // res.send(body);
-        res.send(body.split('&')[0].split('=')[1]);
+        let accessToken = res.send(body.split('&')[0].split('=')[1]);
+        // request.post('https://graph.facebook.com/'+id+'/feed', )
+        request.post({ url: 'https://graph.facebook.com/' + id + '/feed', form: { message: 'testing message', access_token: accessToken } }, function (err, httpResponse, body) { 
+            res.send('發文!!');
+        });
     });
 
 });
