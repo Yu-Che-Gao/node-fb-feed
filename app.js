587331ac8d4b0af59c7c9d4efa2968af82288f84
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-04 17:24:41 +0800

    設定為長時任務

--- app.js
+++ app.js
@@ -96,9 +96,24 @@ app.listen(port, function () {
     console.log('listening on port ' + port);
 });
 
-var rule = new schedule.RecurrenceRule();
-rule.second = 0;
-var j = schedule.scheduleJob(rule, function () {
+// var rule = new schedule.RecurrenceRule();
+// rule.second = 0;
+// var j = schedule.scheduleJob(rule, function () {
+//     console.log('The answer to life, the universe, and everything!');
+//     let dt = timeZone.getTimeZone(8);
+//     let strdate1 = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + " " + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+
+//     let dt2 = new Date();
+//     dt2.setTime(dt.getTime() + 1000 * 60);
+//     let strdate2 = dt2.getFullYear() + "-" + (dt2.getMonth() + 1) + "-" + dt2.getDate() + " " + dt2.getHours() + ":" + dt.getMinutes() + ":" + dt2.getSeconds();
+//     console.log(strdate1);
+//     console.log(strdate2);
+//     // let sql = "SELECT * FROM `pofeed` WHERE `po_time`='"+strdate1+"'";
+//     let sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`>='" + strdate1 + "' AND `po_time`<='" + strdate2 + "'";
+//     mysql.getUpdate(sql);
+// });
+
+setInterval(function() {
     console.log('The answer to life, the universe, and everything!');
     let dt = timeZone.getTimeZone(8);
     let strdate1 = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + " " + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
@@ -109,6 +124,6 @@ var j = schedule.scheduleJob(rule, function () {
     console.log(strdate1);
     console.log(strdate2);
     // let sql = "SELECT * FROM `pofeed` WHERE `po_time`='"+strdate1+"'";
-    let sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`>='" + strdate1 + "' AND `po_time`<='" + strdate2 + "'";
-    mysql.getUpdate(sql);
-});
+    // let sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`>='" + strdate1 + "' AND `po_time`<='" + strdate2 + "'";
+    // mysql.getUpdate(sql);
+}, 1000);
