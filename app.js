b0a45133d99869342c0e2b3e83515f6dc18de8ad
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-08-04 16:58:40 +0800

    更新時間條件

--- app.js
+++ app.js
@@ -102,10 +102,12 @@ var j = schedule.scheduleJob(rule, function () {
     console.log('The answer to life, the universe, and everything!');
     let dt = timeZone.getTimeZone(8);
     let strdate1 = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + " " + dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
-    let strdate2 = dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate() + " " + dt.getHours() + ":" + (dt.getMinutes() + 1) + ":" + dt.getSeconds();
+
+    let dt2=dt.setTime(dt.getTime+1000*60);
+    let strdate2 = dt2.getFullYear() + "-" + (dt2.getMonth() + 1) + "-" + dt2.getDate() + " " + dt2.getHours() + ":" + dt.getMinutes() + ":" + dt2.getSeconds();
     console.log(strdate1);
     console.log(strdate2);
     // let sql = "SELECT * FROM `pofeed` WHERE `po_time`='"+strdate1+"'";
-    let sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`='" + strdate1 + "0'";
+    let sql = "UPDATE `pofeed` SET `content`='success' WHERE `po_time`>='" + strdate1 + "' AND `po_time`<='" + strdate2 + "'";
     mysql.getUpdate(sql);
 });
